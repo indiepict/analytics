72d81e43a61b8d18698c3f0c747e85997b71adc4
indiepict <amazon32@ya.ru>
2017-01-23 20:49:53 +0200

    Create analytics.js

---
+++ analytics.js
@@ -0,0 +1,12 @@
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-50769742-1', 'auto');
+  ga('send', 'pageview');
+if (!document.referrer ||
+     document.referrer.split('/')[2].indexOf(location.hostname) != 0)
+ setTimeout(function(){
+ ga('send', 'event', 'new visitir', location.pathname);
+ }, 15000);
